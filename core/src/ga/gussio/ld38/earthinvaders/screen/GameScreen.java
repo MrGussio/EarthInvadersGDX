a643bdc11733cb0689f2a16a8a53a25734a70b6f
MrGussio <jellehulter@hotmail.com>
2017-04-30 19:33:19 +0200

    Rendering the earth asset instead of drawing a shape.

--- core/src/ga/gussio/ld38/earthinvaders/screen/GameScreen.java
+++ core/src/ga/gussio/ld38/earthinvaders/screen/GameScreen.java
@@ -1,8 +1,10 @@
 package ga.gussio.ld38.earthinvaders.screen;
 
-import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -18,7 +20,7 @@ import ga.gussio.ld38.earthinvaders.math.Circle;
 public class GameScreen extends Screen{
 
     public static Circle earth;
-
+    public Sprite earthTexture;
     public static CopyOnWriteArrayList<Entity> entities = new CopyOnWriteArrayList<Entity>();
 
     public static int health = 100;
@@ -30,29 +32,31 @@ public class GameScreen extends Screen{
         viewport.apply();
         camera.position.set(Game.WIDTH/2, Game.HEIGHT/2, 0);
         camera.update();
+        earthTexture = new Sprite(new Texture(Gdx.files.internal("world.png")));
         earth = new Circle((float) (Game.WIDTH/2-Game.HEIGHT*0.2), (float) (Game.HEIGHT/2-Game.HEIGHT*0.2), (float) (Game.HEIGHT*0.2)); //shaperenderer werkt hier anders, rendert vanuit het midden, dus radius niet van x/y afhalen.
         entities.add(new Player());
         entities.add(new Meteorite());
     }
 
     @Override
-    public void render(SpriteBatch g, ShapeRenderer sr) {
-        g.setProjectionMatrix(camera.combined);
+    public void render(SpriteBatch sb, ShapeRenderer sr) {
+        sb.setProjectionMatrix(camera.combined);
         sr.setAutoShapeType(true);
         sr.setProjectionMatrix(camera.combined);
-        sr.begin();
-        sr.setProjectionMatrix(camera.combined);
-        sr.set(ShapeRenderer.ShapeType.Filled);
         //actual rendering
+        sr.begin();
+        sr.set(ShapeRenderer.ShapeType.Filled);
+        //rendering background
         sr.setColor(new Color(23/255f, 23/255f, 23/255f, 1f));
         sr.rect(0, 0, Game.WIDTH, Game.HEIGHT);
-
-        sr.setColor(Color.WHITE);
-        sr.circle(earth.getXCenter(), earth.getYCenter(), earth.getRadius());
-
         sr.end();
+
+        //rendering earth
+        sb.begin();
+        sb.draw(earthTexture, earth.getX(), earth.getY(), earth.getRadius()*2, earth.getRadius()*2);
+        sb.end();
         for(Entity e : entities){
-            e.render(g, sr);
+            e.render(sb, sr);
         }
     }
 
