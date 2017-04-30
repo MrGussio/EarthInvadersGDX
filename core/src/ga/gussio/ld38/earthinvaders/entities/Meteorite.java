e7b1a97587ae3857ee398933c2b836197de9e652
MrGussio <jellehulter@hotmail.com>
2017-04-30 18:50:59 +0200

    Added Meteorite.

---
+++ core/src/ga/gussio/ld38/earthinvaders/entities/Meteorite.java
@@ -0,0 +1,91 @@
+package ga.gussio.ld38.earthinvaders.entities;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.awt.Rectangle;
+import java.util.Random;
+
+import ga.gussio.ld38.earthinvaders.Game;
+import ga.gussio.ld38.earthinvaders.math.Circle;
+import ga.gussio.ld38.earthinvaders.screen.GameScreen;
+
+public class Meteorite extends Entity {
+
+    private int radius = 25;
+    private int speed = 3;
+    private double velX, velY;
+    private double health;
+    private double rotationSpeed = 1;
+    private double rotation = 0;
+    private Circle collision;
+
+    public Meteorite() {
+        super(0, 0);
+        Random r = new Random();
+        int direction = r.nextInt(360+1);
+        health = r.nextInt(2)+3;
+
+        double tempVelY = 10*Math.cos(Math.toRadians(direction));
+        double tempVelX = 10*Math.sin(Math.toRadians(direction));
+
+        if(r.nextInt(2) == 0)
+            tempVelX*=-1;
+        if(r.nextInt(2) == 0)
+            tempVelY*=-1;
+        Rectangle collision = new Rectangle((int) GameScreen.earth.getXCenter(), (int)GameScreen.earth.getYCenter(), radius*2, radius*2);
+        Rectangle screen = new Rectangle(0, 0, Game.WIDTH, Game.HEIGHT);
+        while(collision.intersects(screen)){
+            collision.translate((int) tempVelX, (int) tempVelY);
+        }
+        x = (float) collision.getX();
+        y = (float) collision.getY();
+        float xSpeed = (Game.WIDTH/2-x) / 1.0f;
+        float ySpeed = (Game.HEIGHT/2-y) / 1.0f;
+        float factor = (float) (speed / Math.sqrt(xSpeed * xSpeed + ySpeed * ySpeed));
+        xSpeed *= factor;
+        ySpeed *= factor;
+
+        velX = xSpeed;
+        velY = ySpeed;
+        this.collision = new Circle(x, y, radius);
+    }
+
+    @Override
+    public void render(SpriteBatch sb, ShapeRenderer sr) {
+        sr.begin();
+        sr.setColor(Color.YELLOW);
+//        sr.rotate(Math.toRadians(rotation), collision.getXCenter(), collision.getYCenter());
+        sr.circle(x, y, collision.getRadius()*2);
+//        sr.rotate(-Math.toRadians(rotation), collision.getXCenter(), collision.getYCenter());
+        sr.end();
+    }
+
+    @Override
+    public void tick() {
+        x += velX;
+        y += velY;
+        collision.setX(x);
+        collision.setY(y);
+        if(collision.hasCollision(GameScreen.earth)){
+            System.out.println("test");
+            GameScreen.damageEarth((int)health*3);
+//            Game.playSound("METEOR1.wav", false, -20);
+            destroy();
+        }
+        rotation += rotationSpeed;
+    }
+
+    public void damage(){
+        health--;
+        if(health <= 0){
+            destroy();
+        }
+    }
+
+    public void destroy(){
+//        GameScreen.entities.add(new Explosion((int) collision.getXCenter(), (int) collision.getYCenter()));
+        GameScreen.entities.remove(this);
+    }
+}
--- core/src/ga/gussio/ld38/earthinvaders/screen/GameScreen.java
+++ core/src/ga/gussio/ld38/earthinvaders/screen/GameScreen.java
@@ -11,6 +11,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import ga.gussio.ld38.earthinvaders.Game;
 import ga.gussio.ld38.earthinvaders.entities.Entity;
+import ga.gussio.ld38.earthinvaders.entities.Meteorite;
 import ga.gussio.ld38.earthinvaders.entities.Player;
 import ga.gussio.ld38.earthinvaders.math.Circle;
 
@@ -20,6 +21,9 @@ public class GameScreen extends Screen{
 
     public static CopyOnWriteArrayList<Entity> entities = new CopyOnWriteArrayList<Entity>();
 
+    public static int health = 100;
+    private static int dmgAnimation = 0;
+
     public GameScreen() {
         camera = new OrthographicCamera();
         viewport = new FitViewport(Game.WIDTH, Game.HEIGHT, camera);
@@ -28,6 +32,7 @@ public class GameScreen extends Screen{
         camera.update();
         earth = new Circle((float) (Game.WIDTH/2), (float) (Game.HEIGHT/2), (float) (Game.HEIGHT*0.2)); //shaperenderer werkt hier anders, rendert vanuit het midden, dus radius niet van x/y afhalen.
         entities.add(new Player());
+        entities.add(new Meteorite());
     }
 
     @Override
@@ -56,10 +61,18 @@ public class GameScreen extends Screen{
         for(Entity e : entities){
             e.tick();
         }
+        if(dmgAnimation > 0){
+            health--;
+            dmgAnimation--;
+        }
     }
 
     @Override
     public void dispose() {
 
+    }
+
+    public static void damageEarth(int hits){
+        dmgAnimation+=hits;
     }
 }
