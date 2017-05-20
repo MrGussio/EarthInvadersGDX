9aeb7156eee69ef293d74dc61c3facb7a8ede8ba
MrGussio <jellehulter@hotmail.com>
2017-05-20 15:25:12 +0200

    Added different constructor methods for initialization of width, height and rotation.

--- core/src/ga/gussio/ld38/earthinvaders/buttons/Button.java
+++ core/src/ga/gussio/ld38/earthinvaders/buttons/Button.java
@@ -10,34 +10,46 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Button {
 
-    protected int x, y, width, height;
+    protected int x, y, width, height, rotation;
     protected float scale = 1.0f;
     protected String filepath;
     protected Sprite img;
     protected Rectangle collision;
     public boolean clicked;
 
-    public Button(int x, int y, String filepath){
+    public Button(int x, int y, String filepath) {
         this.x = x;
         this.y = y;
         this.filepath = filepath;
         img = new Sprite(new Texture(Gdx.files.internal(filepath)));
         this.width = img.getTexture().getWidth();
         this.height = img.getTexture().getHeight();
+        this.rotation = 0;
         collision = new Rectangle(x, y, width, height);
     }
 
-    public void render(SpriteBatch sb, ShapeRenderer sr){
+    public Button(int x, int y, int width, int height, String filepath) {
+        this(x, y, filepath);
+        this.width = width;
+        this.height = height;
+    }
+
+    public Button(int x, int y, int width, int height, int rotationInDegr, String filepath) {
+        this(x, y, width, height, filepath);
+        this.rotation = rotation;
+    }
+
+    public void render(SpriteBatch sb, ShapeRenderer sr) {
         sb.begin();
         sb.draw(img, x, y, width, height);
         sb.end();
     }
 
-    public void tick(){
+    public void tick() {
 
     }
 
-    public void dispose(){
+    public void dispose() {
         img.getTexture().dispose();
     }
 
@@ -63,8 +75,8 @@ public class Button {
 
     public void setScale(float scale) {
         this.scale = scale;
-        width = (int) (img.getWidth()*scale);
-        height = (int) (img.getHeight()*scale);
+        width = (int) (img.getWidth() * scale);
+        height = (int) (img.getHeight() * scale);
         collision.set(x, y, width, height);
     }
 
@@ -80,8 +92,8 @@ public class Button {
         return filepath;
     }
 
-    public void click(Vector2 p){
-        if(collision.contains(p)){
+    public void click(Vector2 p) {
+        if (collision.contains(p)) {
             this.clicked = true;
         }
 
